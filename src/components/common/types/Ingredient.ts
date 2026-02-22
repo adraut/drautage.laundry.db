01670cd14789bf2178445c13d961d7432a4c3e83
adraut <adraut@users.noreply.github.com>
2026-02-22 13:41:03 -0600

    Fix ingredients.

--- src/components/common/types/Ingredient.ts
+++ src/components/common/types/Ingredient.ts
@@ -27,7 +27,13 @@ export enum Ingredient {
   Ethanol = 'Ethanol',
   Ethanolamine = 'Ethanolamine',
   FluorescentBrightener71 = 'Fluorescent Brightener 71',
-  Fragrance = 'Fragrance', // also listed as: Long Lasting Fragrance, Long Lasting Frgrance
+  /**
+  Fragrance is a catch-all term that can refer to a wide variety of different fragrance ingredients.
+  It is often used as a generic term for any ingredient that is added to a product to provide a scent.
+  In some cases, it may be used to refer to a specific fragrance ingredient, but in most cases, it is used as a general term for any fragrance ingredient.
+  Synonyms: Long Lasting Fragrance
+  **/
+  Fragrance = 'Fragrance',
   Glycerin = 'Glycerin',
   HydrogenatedCastorOil = 'Hydrogenated Castor Oil',
   HydrophobicallyModifiedAcrylateStyreneCopolymer = 'Hydrophobically Modified Acrylate Styrene Copolymer',
@@ -36,9 +42,16 @@ export enum Ingredient {
   LinearAlcoholEthoxylates = 'Linear Alcohol Ethoxylates',
   Lipase = 'Lipase',
   Mannanase = 'Mannanase',
-  MEAC10_16Alkylbenzenesulfonate = 'MEA C10-16 Alkylbenzenesulfonate', // also listed as: Mea-Dodecylbenzenesulfonate
+  MEAC10_16Alkylbenzenesulfonate = 'MEA C10-16 Alkylbenzenesulfonate',
   MEAC12_18FattyAcidsSalt = 'MEA Salts of C12-18 Fatty Acids',
+  /**
+   * MEA-citrate is a common ingredient in laundry detergents that is used as a chelating agent and a pH adjuster.
+   * It is a combination of Monoethanolamine (MEA) and citric acid, which helps to improve the performance of the detergent by binding to metal ions and preventing them from interfering with the cleaning process.
+   * MEA-citrate can also help to soften water and improve the effectiveness of the detergent.
+   * Synonyms: Ethanolamine Citrate
+   */
   MEACitrate = 'MEA-citrate', // also listed as: Ethanolamine Citrate
+  MEADedecylbenzenesulfonate = 'MEA-Dodecylbenzenesulfonate',
   MEALaurethSulfate = 'MEA Laureth Sulfate',
   MethoxypolyoxymethyleneMelamine = 'Methoxypolyoxymethylene Melamine',
   MethylDiTButylHydroxyhydrocinnamate = 'Methyl di-t-butyl hydroxyhydrocinnamate',
@@ -51,7 +64,11 @@ export enum Ingredient {
   PhenylpropylEthylMethicone = 'Phenylpropyl Ethyl Methicone',
   PigmentBlue15 = 'Pigment Blue 15',
   PigmentRed5 = 'Pigment Red 5',
-  PolyethyleneimineAlkoxylated = 'Polyethyleneimine Alkoxylated', // also listed as: Polyethylene Imine Ethoxylate, Polyethlylene Imine Ethoxylate
+  PolyethyleneimineAlkoxylated = 'Polyethyleneimine Alkoxylated',
+  /**
+   * This is a specific type of Polyethyleneimine Alkoxylated.
+   */
+  PolyethlyleneImineEthoxylate = 'Polyethylene Imine Ethoxylate',
   PolyoxyalkyleneSubstitutedChromophoreBlue = 'Polyoxyalkylene Substituted Chromophore (Blue)',
   PolyoxyalkyleneSubstitutedChromophoreCyan = 'Polyoxyalkylene Substituted Chromophore (Cyan)',
   PolyoxyalkyleneSubstitutedChromophoreViolet = 'Polyoxyalkylene Substituted Chromophore (Violet)',
